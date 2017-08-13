857444cd6479134f5913271b7040204cb2259f0b
Robert Ipach <robert.ipach@gmail.com>
2017-08-13 13:11:11 +0200

    Don't exclude grace notes from HMN

--- libmscore/cmd.cpp
+++ libmscore/cmd.cpp
@@ -2901,8 +2901,9 @@ void Score::cmdHamburgMusicNotation(bool showNotenames)
       foreach (Element* e, selection().elements()) {
             if (e->type() == ElementType::NOTE) {
                   Note* n = toNote(e);
-                  if (n->noteType() != NoteType::NORMAL)
+                  if (n->noteType() == NoteType::INVALID)
                         continue;
+
                   Chord* c = n->chord();
 
                   // check for duplicates (chords with multiple notes)
